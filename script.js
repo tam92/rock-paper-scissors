0bb6dda5aa083afeab7a12758ab712142f8f7e51
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-12 15:56:49 +0200

    User interface added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Game of rock paper scissors against the computer
+let playerCounter = 0;
+let computerCounter = 0;
+
 game(); // We play a game
 
 // ----- FUNCTIONS -----
-
 // Function to get the computer's choice:
 function getComputerChoice() {
     // We get a random number between 0 and 2
@@ -13,66 +15,115 @@ function getComputerChoice() {
 
 // Function that plays a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
+    const displayDiv = document.getElementById("resultsDisplay");
+    let currentResult;
+
     // returns 0 if the player lost, returns 1 if the player won, returns 2 if it's a tie
     if (playerSelection === computerSelection) {
-        console.log(`It's a tie! You both choose ${playerSelection}!`);
+        displayDiv.textContent = `It's a tie! You both choose ${playerSelection}!`;
         return 2;
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log(`You lose! Paper beats Rock!`);
+        displayDiv.textContent = `You lose! Paper beats Rock!`;
         return 0;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win! Paper beats Rock!");
+        displayDiv.textContent = "You win! Paper beats Rock!";
         return 1;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log(`You lose! Rock beats Scissors!`);
+        displayDiv.textContent = `You lose! Rock beats Scissors!`;
         return 0;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(`You win! Rock beats Scissors!`);
+        displayDiv.textContent = `You win! Rock beats Scissors!`;
         return 1;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log(`You lose! Scissors beat Paper!`);
+        displayDiv.textContent = `You lose! Scissors beat Paper!`;
         return 0;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(`You win! Scissors beat Paper!`);
+        displayDiv.textContent = `You win! Scissors beat Paper!`;
         return 1;
     }
 }
 
-// Function that plays a 5 round game and keeps score and reports a winner or looser at the end
 function game() {
-    let playerCounter = 0;
-    let computerCounter = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock Paper Scissors").toLowerCase();
         let computerSelection = getComputerChoice();
-
-        console.log(`You choose ${playerSelection}. The computer chooses ${computerSelection}`);
-
-        let currentGame = playRound(playerSelection, computerSelection);
-        
+        const choicesDiv = document.getElementById("choicesDisplay");
+        let currentGame;
+        const totalDiv = document.getElementById("totalScoreDisplay");
+    
+        // Btn
+    const rockBtn = document.querySelector('#rockBtn');
+    rockBtn.addEventListener('click', () => {
+        playerSelection = "rock";
+        computerSelection = getComputerChoice();
+        choicesDiv.textContent = `You choose ${playerSelection}. The computer chooses ${computerSelection}`;
+        currentGame = playRound(playerSelection, computerSelection);
         switch (currentGame) {
             case 0: // player lost, computer won
                 computerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
                 break;
             case 1: // player won, computer lost
                 playerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
                 break;
             case 2: // it's a tie
-                playerCounter++;
-                computerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
                 break;
         }
-    }
+        checkScore(computerCounter, playerCounter);
+    });
 
-    console.log(`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`);
+    const paperBtn = document.querySelector('#paperBtn');
+    paperBtn.addEventListener('click', () => {
+        playerSelection = "paper";
+        computerSelection = getComputerChoice();
+        choicesDiv.textContent = `You choose ${playerSelection}. The computer chooses ${computerSelection}`;
+        currentGame = playRound(playerSelection, computerSelection);
+        switch (currentGame) {
+            case 0: // player lost, computer won
+                computerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+            case 1: // player won, computer lost
+                playerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+            case 2: // it's a tie
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+        }
+        checkScore(computerCounter, playerCounter);
+    });
 
-    // Report a winner at the end
-    if (playerCounter > computerCounter) {
-        console.log("You win!");
-    } if (computerCounter > playerCounter) {
-        console.log("The computer wins!");
-    } else {
-        console.log("It's a tie!");
+    const scissorsBtn = document.querySelector('#scissorsBtn');
+    scissorsBtn.addEventListener('click', () => {
+        playerSelection = "scissors";
+        computerSelection = getComputerChoice();
+        choicesDiv.textContent = `You choose ${playerSelection}. The computer chooses ${computerSelection}`;
+        currentGame = playRound(playerSelection, computerSelection);
+        switch (currentGame) {
+            case 0: // player lost, computer won
+                computerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+            case 1: // player won, computer lost
+                playerCounter++;
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+            case 2: // it's a tie
+                totalDiv.textContent =`TOTAL SCORES: player ${playerCounter}, computer ${computerCounter}`;
+                break;
+        }
+        checkScore(computerCounter, playerCounter);
+    });
+}
+
+function checkScore(computerCounter, playerCounter) {
+    if (computerCounter == 5 || playerCounter == 5) {
+        resetScore();
     }
+}
+
+function resetScore() {
+    computerCounter = 0;
+    playerCounter = 0;
 }
